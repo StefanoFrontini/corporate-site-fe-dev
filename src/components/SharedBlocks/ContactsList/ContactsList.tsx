463f32cdd3abde7946ca2469d5659c24b2ce35ef
Stefano Frontini <stefanofrontini75@gmail.com>
2025-12-09 11:29:58 +0100

    contactsList

--- src/components/SharedBlocks/ContactsList/ContactsList.tsx
+++ src/components/SharedBlocks/ContactsList/ContactsList.tsx
@@ -31,7 +31,7 @@ const ContactsList = ({
   return (
     <section
       className={`block --block-contacts-list contacts-list${
-        !title ? ' --no-title' : ''
+        !title ? ' no-title' : ''
       }`}
     >
       <div className="container-fluid">
